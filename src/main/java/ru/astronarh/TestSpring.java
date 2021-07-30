56d35e317ffdd2f213762f8329269323d5249695
astronarh <astronarh@gmail.com>
2021-07-30 09:27:25 +0300

    Урок 7

--- src/main/java/ru/astronarh/TestSpring.java
+++ src/main/java/ru/astronarh/TestSpring.java
@@ -6,10 +6,28 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
 
-        musicPlayer.playMusic();
-        System.out.println("Name: " + musicPlayer.getName() + ", volume: " + musicPlayer.getVolume());
+        boolean compare = musicPlayer1 == musicPlayer2;
+
+        System.out.println(compare);
+
+        System.out.println(musicPlayer1);
+        System.out.println(musicPlayer2);
+
+        musicPlayer1.setVolume(10);
+
+        System.out.println(compare);
+
+        System.out.println(musicPlayer1);
+        System.out.println(musicPlayer2);
+
+        musicPlayer1.playMusic();
+        System.out.println("Name: " + musicPlayer1.getName() + ", volume: " + musicPlayer1.getVolume());
+
+        musicPlayer2.playMusic();
+        System.out.println("Name: " + musicPlayer2.getName() + ", volume: " + musicPlayer2.getVolume());
 
         context.close();
     }
